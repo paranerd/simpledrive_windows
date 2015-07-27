d8e834425807472504fd414ff917823b2df090b2
paranerd <in.my.digital.world@gmail.com>
2015-07-27 21:06:32 +0200

    first release

---
+++ sd_client/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace sd_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.server_label = new System.Windows.Forms.Label();
+            this.user_label = new System.Windows.Forms.Label();
+            this.pass_label = new System.Windows.Forms.Label();
+            this.folder_label = new System.Windows.Forms.Label();
+            this.server_input = new System.Windows.Forms.TextBox();
+            this.user_input = new System.Windows.Forms.TextBox();
+            this.pass_input = new System.Windows.Forms.TextBox();
+            this.folder_input = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.browse = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.synchronizeNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simpleDriveInExplorerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // server_label
+            // 
+            this.server_label.AutoSize = true;
+            this.server_label.Location = new System.Drawing.Point(13, 166);
+            this.server_label.Name = "server_label";
+            this.server_label.Size = new System.Drawing.Size(59, 20);
+            this.server_label.TabIndex = 1;
+            this.server_label.Text = "Server:";
+            // 
+            // user_label
+            // 
+            this.user_label.AutoSize = true;
+            this.user_label.Location = new System.Drawing.Point(13, 199);
+            this.user_label.Name = "user_label";
+            this.user_label.Size = new System.Drawing.Size(47, 20);
+            this.user_label.TabIndex = 2;
+            this.user_label.Text = "User:";
+            // 
+            // pass_label
+            // 
+            this.pass_label.AutoSize = true;
+            this.pass_label.Location = new System.Drawing.Point(13, 238);
+            this.pass_label.Name = "pass_label";
+            this.pass_label.Size = new System.Drawing.Size(82, 20);
+            this.pass_label.TabIndex = 3;
+            this.pass_label.Text = "Password:";
+            // 
+            // folder_label
+            // 
+            this.folder_label.AutoSize = true;
+            this.folder_label.Location = new System.Drawing.Point(13, 282);
+            this.folder_label.Name = "folder_label";
+            this.folder_label.Size = new System.Drawing.Size(58, 20);
+            this.folder_label.TabIndex = 4;
+            this.folder_label.Text = "Folder:";
+            // 
+            // server_input
+            // 
+            this.server_input.Location = new System.Drawing.Point(128, 163);
+            this.server_input.Name = "server_input";
+            this.server_input.Size = new System.Drawing.Size(196, 26);
+            this.server_input.TabIndex = 5;
+            // 
+            // user_input
+            // 
+            this.user_input.Location = new System.Drawing.Point(128, 196);
+            this.user_input.Name = "user_input";
+            this.user_input.Size = new System.Drawing.Size(196, 26);
+            this.user_input.TabIndex = 6;
+            // 
+            // pass_input
+            // 
+            this.pass_input.Location = new System.Drawing.Point(128, 235);
+            this.pass_input.Name = "pass_input";
+            this.pass_input.PasswordChar = '*';
+            this.pass_input.Size = new System.Drawing.Size(196, 26);
+            this.pass_input.TabIndex = 7;
+            // 
+            // folder_input
+            // 
+            this.folder_input.AutoSize = true;
+            this.folder_input.Location = new System.Drawing.Point(124, 282);
+            this.folder_input.Name = "folder_input";
+            this.folder_input.Size = new System.Drawing.Size(0, 20);
+            this.folder_input.TabIndex = 8;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(268, 277);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(56, 31);
+            this.browse.TabIndex = 9;
+            this.browse.Text = "...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(180, 323);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(144, 49);
+            this.connect.TabIndex = 10;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(13, 363);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 20);
+            this.status.TabIndex = 11;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.synchronizeNowToolStripMenuItem,
+            this.simpleDriveInExplorerToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(279, 94);
+            // 
+            // synchronizeNowToolStripMenuItem
+            // 
+            this.synchronizeNowToolStripMenuItem.Name = "synchronizeNowToolStripMenuItem";
+            this.synchronizeNowToolStripMenuItem.Size = new System.Drawing.Size(278, 30);
+            this.synchronizeNowToolStripMenuItem.Text = "Synchronize Now";
+            this.synchronizeNowToolStripMenuItem.Click += new System.EventHandler(this.synchronizeNowToolStripMenuItem_Click);
+            // 
+            // simpleDriveInExplorerToolStripMenuItem
+            // 
+            this.simpleDriveInExplorerToolStripMenuItem.Name = "simpleDriveInExplorerToolStripMenuItem";
+            this.simpleDriveInExplorerToolStripMenuItem.Size = new System.Drawing.Size(278, 30);
+            this.simpleDriveInExplorerToolStripMenuItem.Text = "simpleDrive in Explorer";
+            this.simpleDriveInExplorerToolStripMenuItem.Click += new System.EventHandler(this.simpleDriveInExplorerToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(278, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.connect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 401);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.folder_input);
+            this.Controls.Add(this.pass_input);
+            this.Controls.Add(this.user_input);
+            this.Controls.Add(this.server_input);
+            this.Controls.Add(this.folder_label);
+            this.Controls.Add(this.pass_label);
+            this.Controls.Add(this.user_label);
+            this.Controls.Add(this.server_label);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "simpleDrive";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label server_label;
+        private System.Windows.Forms.Label user_label;
+        private System.Windows.Forms.Label pass_label;
+        private System.Windows.Forms.Label folder_label;
+        private System.Windows.Forms.TextBox server_input;
+        private System.Windows.Forms.TextBox user_input;
+        private System.Windows.Forms.TextBox pass_input;
+        private System.Windows.Forms.Label folder_input;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem synchronizeNowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simpleDriveInExplorerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ sd_client/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Diagnostics;
+
+namespace sd_client
+{
+    public partial class Form1 : Form
+    {
+        static string config_path = Directory.GetParent(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\shared.config";
+        static ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+        static Configuration config;
+        private static System.Timers.Timer timer = new System.Timers.Timer();
+
+        static string userdir = "";
+        static bool sync_in_progress = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            notifyIcon1.Text = "simpleDrive Sync Client";
+
+            fileMap.ExeConfigFilename = config_path;
+            config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            if (!File.Exists(config_path))
+            {
+                status.Text = "Config not found";
+                blockText(true);
+                return;
+            }
+
+            // Preset values
+            server_input.Text = (config.AppSettings.Settings["server"] != null) ? config.AppSettings.Settings["server"].Value : "";
+            user_input.Text = (config.AppSettings.Settings["user"] != null) ? config.AppSettings.Settings["user"].Value : "";
+            pass_input.Text = (config.AppSettings.Settings["pass"] != null) ? config.AppSettings.Settings["pass"].Value : "";
+            folder_input.Text = (config.AppSettings.Settings["folder"] != null) ? new FileInfo(config.AppSettings.Settings["folder"].Value).Name : "";
+            userdir = (config.AppSettings.Settings["folder"] != null) ? config.AppSettings.Settings["folder"].Value : "";
+
+            timer.Interval = 3000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+
+            if(server_input.Text != "" && user_input.Text != "" && pass_input.Text != "" && userdir != "")
+            {
+                timer.Start();
+            }
+
+            notifyIcon1.BalloonTipTitle = "simpleDrive Sync Client";
+            if (timer.Enabled)
+            {
+                blockText(true);
+                status.ForeColor = Color.Green;
+                status.Text = "Sync service running...";
+                connect.Text = "Disconnect";
+
+                notifyIcon1.BalloonTipText = "Sync client started";
+            }
+            else
+            {
+                connect.Text = "Connect";
+                notifyIcon1.BalloonTipText = "Setup your connection";
+            }
+            notifyIcon1.ShowBalloonTip(5000);
+        }
+
+        public async void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            await sync();
+        }
+
+        public async Task sync()
+        {
+            if (!sync_in_progress)
+            {
+                notifyIcon1.Text = "Sync in progress...";
+                sync_in_progress = true;
+                if (!File.Exists(config_path))
+                {
+                    notifyIcon1.Text = "simpleDrive Sync Client";
+                    return;
+                }
+
+                KeyValueConfigurationElement server = config.AppSettings.Settings["server"];
+                KeyValueConfigurationElement user = config.AppSettings.Settings["user"];
+                KeyValueConfigurationElement pass = config.AppSettings.Settings["pass"];
+                KeyValueConfigurationElement folder = config.AppSettings.Settings["folder"];
+                if (server.Value == null && user.Value == null && pass.Value == null && folder.Value == null)
+                {
+                    notifyIcon1.Text = "Config not complete";
+                    status.Text = "Config not complete";
+                    return;
+                }
+                if (!Directory.Exists(folder.Value))
+                {
+                    notifyIcon1.Text = "Sync folder does not exist";
+                }
+                await simpledrive.sync(server.Value, user.Value, pass.Value, folder.Value);
+                notifyIcon1.Text = "Last sync " + DateTime.Now.ToString("dd.MM.yyy - hh:mm");
+                sync_in_progress = false;
+            }
+        }
+
+        private void writeSettings(Dictionary<string, string> settings)
+        {
+            foreach (KeyValuePair<string, string> entry in settings)
+            {
+                config.AppSettings.Settings.Remove(entry.Key);
+                config.AppSettings.Settings.Add(entry.Key, entry.Value);
+            }
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        private void blockText(bool block)
+        {
+            server_input.ReadOnly = block;
+            user_input.ReadOnly = block;
+            pass_input.ReadOnly = block;
+            if (block)
+            {
+                server_input.BorderStyle = BorderStyle.None;
+                user_input.BorderStyle = BorderStyle.None;
+                pass_input.BorderStyle = BorderStyle.None;
+            }
+            else
+            {
+                server_input.BorderStyle = BorderStyle.FixedSingle;
+                user_input.BorderStyle = BorderStyle.FixedSingle;
+                pass_input.BorderStyle = BorderStyle.FixedSingle;
+            }
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                userdir = folderBrowserDialog1.SelectedPath;
+                folder_input.Text = new FileInfo(userdir).Name;
+            }
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(config_path))
+            {
+                status.Text = "Config not found";
+                blockText(true);
+                return;
+            }
+            status.Text = "";
+            status.ForeColor = Color.Red;
+
+            if (timer.Enabled)
+            {
+                status.Text = "Sync service stopped";
+                connect.Text = "Connect";
+                blockText(false);
+                timer.Stop();
+            }
+            else
+            {
+                if (server_input.Text == "" || user_input.Text == "" || pass_input.Text == "" || userdir == "")
+                {
+                    status.Text = "No blank fields!";
+                    return;
+                }
+
+                blockText(true);
+                status.Text = "Connecting...";
+                string login = simpledrive.login(server_input.Text, user_input.Text, pass_input.Text);
+                if (login == null)
+                {
+                    status.Text = "Connection error";
+                    blockText(false);
+                }
+                else if (login == "1")
+                {
+                    var settings = new Dictionary<string, string>
+                    {
+                        { "server", server_input.Text },
+                        { "user", user_input.Text },
+                        { "pass", pass_input.Text },
+                        { "folder", userdir }
+                    };
+                    writeSettings(settings);
+                    timer.Start();
+                    status.ForeColor = Color.Green;
+                    status.Text = "Sync service running...";
+                    connect.Text = "Disconnect";
+                    simpledrive.create_fav_link(userdir);
+                }
+                else
+                {
+                    status.Text = "Login failed";
+                    blockText(false);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Show();
+            }
+        }
+
+        private async void synchronizeNowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            await sync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                this.Hide();
+                e.Cancel = true;
+            }
+        }
+
+        private void simpleDriveInExplorerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", userdir);
+        }
+    }
+}
